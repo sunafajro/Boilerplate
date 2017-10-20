b046ac05df465808083e05554de99cb7f8727cc5
Evgeny Belkin <sunafajro@gmail.com>
2017-10-21 00:22:10 +0300

    ссылки панели навигации получаем из запроса на состояние

--- frontend/src/modules/index.js
+++ frontend/src/modules/index.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
-import navigation from './reducers/navigation';
 import home from './reducers/home';
 import auth from './reducers/auth';
 
 export default combineReducers({
   routing: routerReducer,
-  navigation,
   home,
   auth
 })
