8e5380b7697fdbc80692e2374e0da63d55ec6755
Evgeny Belkin <sunafajro@gmail.com>
2017-10-28 21:07:21 +0300

    добавляем модель со списком офисов школы (потом перенесем в базу) и блок для ее вывода

---
+++ frontend/src/containers/home/components/info-block.js
@@ -0,0 +1,59 @@
+/* @flow */
+import React from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { getState } from '../../../modules/actions/auth';
+
+type Props = {
+  contacts: Array<{
+    id:      string,
+    city:    string,
+    offices: Array<{
+      id:      string,
+      address: string,
+      phone:   string
+    }>
+  }>
+};
+
+class InfoBlock extends React.Component<Props, {}> {
+  render () {
+    let props = this.props;
+    return (
+      <div className="card border-warning">
+        <h5 className="card-header bg-warning text-white">Наши контакты</h5>
+        <div className="card-body">
+          { props.contacts.length ?
+            props.contacts.map(item => {
+              let offices = [];
+              if (item.offices.length) {
+                offices = item.offices.map(office => {
+                  return (
+                    <li key={ office.id }>
+                      { office.address }
+                      <br />Тел. { office.phone }
+                    </li>
+                  );
+                });
+              }
+              return (
+                <div key={ item.id }>
+                  <p><b>{ item.city }:</b></p>
+                  { offices.length ?
+                    <ul>
+                      { offices }
+                    </ul> : '' }
+                </div>
+              );
+            }): '' }
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  contacts: state.auth.contacts
+});
+  
+export default connect(mapStateToProps, null)(InfoBlock);
---
+++ models/Contact.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\base\Model;
+
+class Contact extends Model
+{
+
+    public static function getContacts()
+    {
+        $contacts = [
+            [
+                'id'      => 'cheb',
+                'city'    => 'Чебоксары',
+                'offices' => [
+                    [
+                        'id'      => 'cheb-1',
+                        'address' => 'Московский пр. 17, 6 этаж',
+                        'phone'   => '(8352) 43-96-77'
+                    ],
+                    [
+                        'id'      => 'cheb-2',
+                        'address' => 'пр. Ленина 7, 3 этаж',
+                        'phone'   => '(8352) 23-02-03'
+                    ],
+                    [
+                        'id'      => 'cheb-3',
+                        'address' => 'ул. Университетская 34, 2 этаж',
+                        'phone'   => '(8352) 68-50-90'
+                    ],
+                    [
+                        'id'      => 'cheb-4',
+                        'address' => 'пр. 9 Пятилетки 19/37',
+                        'phone'   => '(8352) 68-50-03'
+                    ],
+                    [
+                        'id'      => 'cheb-5',
+                        'address' => 'пр. М. Горького 12',
+                        'phone'   => '(8352) 68-00-56'
+                    ],
+                    [
+                        'id'      => 'cheb-6',
+                        'address' => 'ул. Н. Смирнова 7',
+                        'phone'   => '(8352) 68-03-45'
+                    ],
+                ]
+            ],
+            [
+                'id'      => 'novcheb',
+                'city'    => 'Новочебоксарск',
+                'offices' => [
+                    [
+                        'id'      => 'novcheb-1',
+                        'address' => 'ул. Пионерская 4/2',
+                        'phone'   => '(8352) 68-00-52'
+                    ]
+                ]
+            ]
+        ];
+
+        return $contacts;
+    }
+}
