d00932881eb814ba091dd10c9fbaae2199fc876b
Evgeny Belkin <sunafajro@gmail.com>
2017-10-28 21:08:03 +0300

    обновляем надпись в подвале

--- frontend/src/containers/footer/index.js
+++ frontend/src/containers/footer/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const Footer = () => (
   <footer className="footer">
     <div className="container">
-      <span className="text-muted">Place sticky footer content here.</span>
+      <span className="text-muted">© Школа иностранных языков "Язык для успеха" 2017</span>
     </div>
   </footer>
 );
