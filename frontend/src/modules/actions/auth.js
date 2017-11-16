0da45aebfca95a8f22312023c7591a8d9f1e9175
Evgeny Belkin <sunafajro@gmail.com>
2017-11-16 23:30:20 +0300

    переработаны дейтвия и редьюсер для flow проверок

--- frontend/src/modules/actions/auth.js
+++ frontend/src/modules/actions/auth.js
@@ -1,165 +1,221 @@
-import md5 from 'js-md5';
+/* @flow */
 
-export const LOGIN = 'LOGIN';
-export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
-export const LOGIN_FAILED = 'LOGIN_FAILED';
+export const LOGIN: string = "LOGIN";
+export const LOGIN_SUCCESS: string = "LOGIN_SUCCESS";
+export const LOGIN_FAILED: string = "LOGIN_FAILED";
 
-export const LOGOUT = 'LOGOUT';
-export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS';
-export const LOGOUT_FAILED = 'LOGOUT_FAILED';
+export const LOGOUT: string = "LOGOUT";
+export const LOGOUT_SUCCESS: string = "LOGOUT_SUCCESS";
+export const LOGOUT_FAILED: string = "LOGOUT_FAILED";
 
-export const GET_STATE = 'GET_STATE';
-export const GET_STATE_SUCCESS = 'GET_STATE_SUCCESS';
-export const GET_STATE_FAILED = 'GET_STATE_FAILED';
+export const GET_STATE: string = "GET_STATE";
+export const GET_STATE_SUCCESS: string = "GET_STATE_SUCCESS";
+export const GET_STATE_FAILED: string = "GET_STATE_FAILED";
 
-export const login = (username, password) => {
+export const UPDATE_STATE: string = "UPDATE_STATE";
+export const UPDATE_STATE_USERNAME: string = "UPDATE_STATE_USERNAME";
+export const UPDATE_STATE_PASSWORD: string = "UPDATE_STATE_PASSWORD";
+export const UPDATE_STATE_VALID: string = "UPDATE_STATE_VALID";
+
+import md5 from "js-md5";
+import type { ThunkAction } from '../types/auth'; 
+
+export const login = (username: string, password: string): ThunkAction => {
   return dispatch => {
     dispatch({
       type: LOGIN
     });
 
     const body = {
-      'LoginForm': {
-         username: username, 
-         password: md5(password) 
+      LoginForm: {
+        username: username,
+        password: md5(password)
       }
     };
 
-    fetch('/api/login',
-    {
-      method: 'POST',
-      accept: 'application/json',
-      credentials: 'include',
+    fetch("/api/login", {
+      method: "POST",
+      accept: "application/json",
+      credentials: "include",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json"
       },
       body: JSON.stringify(body)
     })
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }
-      throw new Error('Ошибка входа!');
-    })
-    .then(result => dispatch(loginSuccess(result)))
-    .catch(err   => dispatch(loginFailed(err)));
-  }
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Ошибка входа!");
+      })
+      .then(result => dispatch(loginSuccess(result)))
+      .catch(error => dispatch(loginFailed(error)));
+  };
 };
 
-export const loginSuccess = (result) => {
+export const loginSuccess = (result: Object): ThunkAction => {
   return dispatch => {
     dispatch({
-      type:     LOGIN_SUCCESS,
+      type: LOGIN_SUCCESS,
       loggedIn: result.loggedIn,
-      profile:  result.profile,
+      profile: result.profile,
       contacts: result.contacts,
       navigation: result.navigation,
-      message:  { type: 'success', text: result.message }
+      message: { type: "success", text: result.message }
     });
-  }
-}
+  };
+};
 
-export const loginFailed = (error) => {
+export const loginFailed = (error: string): ThunkAction => {
   return dispatch => {
     dispatch({
       type: LOGIN_FAILED,
-      message: { type: 'fail', text: error.message }
+      message: { type: "fail", text: error }
     });
-  }
-}
+  };
+};
 
-export const logout = () => {
+export const logout = (): ThunkAction => {
   return dispatch => {
     dispatch({
       type: LOGOUT
     });
 
-    fetch('/api/logout',
-    {
-      method: 'POST',
-      accept: 'application/json',
-      credentials: 'include',
+    fetch("/api/logout", {
+      method: "POST",
+      accept: "application/json",
+      credentials: "include",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json"
       }
     })
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }
-      let r = response.json();
-      throw new Error(r.message ? r.message : 'Внутренняя ошибка сервера!');
-    })
-    .then(result => dispatch(logoutSuccess(result)))
-    .catch(err   => dispatch(logoutFailed(err)));
-  }
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Внутренняя ошибка сервера!");
+      })
+      .then(result => dispatch(logoutSuccess(result)))
+      .catch(error => dispatch(logoutFailed(error)));
+  };
 };
 
-export const logoutSuccess = (result) => {
+export const logoutSuccess = (result: Object): ThunkAction => {
   return dispatch => {
     dispatch({
       type: LOGOUT_SUCCESS,
       contacts: result.contacts,
       navigation: result.navigation,
-      message: { type: 'success', text: result.message }
+      message: { type: "success", text: result.message }
     });
-  }
-}
+  };
+};
 
-export const logoutFailed = (error) => {
+export const logoutFailed = (error: string): ThunkAction => {
   return dispatch => {
     dispatch({
       type: LOGOUT_FAILED,
-      message: { type: 'fail', text: error }
+      message: { type: "fail", text: error }
     });
-  }
-}
+  };
+};
 
-export const getState = () => {
+export const getState = (): ThunkAction => {
   return dispatch => {
     dispatch({
       type: GET_STATE
     });
 
-    fetch('/api/state',
-    {
-      method: 'POST',
-      accept: 'application/json',
-      credentials: 'include',
+    fetch("/api/state", {
+      method: "POST",
+      accept: "application/json",
+      credentials: "include",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json"
       }
     })
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }
-      let r = response.json();
-      throw new Error(r.message ? r.message : 'Внутренняя ошибка сервера!');
-    })
-    .then(result => dispatch(getStateSuccess(result)))
-    .catch(err => dispatch(getStateFailed(err)));
-  }
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Внутренняя ошибка сервера!");
+      })
+      .then(result => dispatch(getStateSuccess(result)))
+      .catch(error => dispatch(getStateFailed(error)));
+  };
 };
 
-export const getStateSuccess = (result) => {
+export const getStateSuccess = (result: Object): ThunkAction => {
   return dispatch => {
     dispatch({
-      type:       GET_STATE_SUCCESS,
-      loggedIn:   result.loggedIn,
-      profile:    result.profile,
-      contacts:   result.contacts,
+      type: GET_STATE_SUCCESS,
+      loggedIn: result.loggedIn,
+      profile: result.profile,
+      contacts: result.contacts,
       navigation: result.navigation,
-      message:    { type: 'success', text: result.message }
+      message: { type: "success", text: result.message },
+      labels: result.labels
     });
-  }
-}
+  };
+};
 
-export const getStateFailed = (error) => {
+export const getStateFailed = (error: string): ThunkAction => {
   return dispatch => {
     dispatch({
       type: GET_STATE_FAILED,
-      message: { type: 'fail', text: error }
+      message: { type: "fail", text: error }
     });
-  }
-}
+  };
+};
+
+export const updateState = (value: string | boolean, key: string): ThunkAction => {
+  return dispatch => {
+    dispatch({
+      type: UPDATE_STATE
+    });
+    switch (key) {
+      case "username":
+        {
+          dispatch(updateStateUsername(value));
+        }
+        break;
+      case "password":
+        {
+          dispatch(updateStatePassword(value));
+        }
+        break;
+      case "valid":
+        {
+          dispatch(updateStateValid(value));
+        }
+        break;
+    }
+  };
+};
+
+export const updateStateUsername = (username: string): ThunkAction => {
+  return dispatch => {
+    dispatch({
+      type: UPDATE_STATE_USERNAME,
+      username
+    });
+  };
+};
+
+export const updateStatePassword = (password: string): ThunkAction => {
+  return dispatch => {
+    dispatch({
+      type: UPDATE_STATE_PASSWORD,
+      password
+    });
+  };
+};
+
+export const updateStateValid = (valid: boolean): ThunkAction => {
+  return dispatch => {
+    dispatch({
+      type: UPDATE_STATE_VALID,
+      valid
+    });
+  };
+};
--- frontend/src/modules/reducers/auth.js
+++ frontend/src/modules/reducers/auth.js
@@ -1,99 +1,162 @@
-export const LOGIN = 'LOGIN';
-export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
-export const LOGIN_FAILED = 'LOGIN_FAILED';
+/* @flow */
 
-export const LOGOUT = 'LOGOUT';
-export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS';
-export const LOGOUT_FAILED = 'LOGOUT_FAILED';
+const DEFAULT_LABELS: Object = {
+  homeBreadcrumbs: "Главная",
+  loginBreadcrumbs: "Вход",
+  loginPageTitle: "Форма входа",
+  formEmptyFieldsAlert: "Поля формы должны быть заполнены!",
+  usernameLabel: "Логин",
+  passwordLabel: "Пароль",
+  submitBtnLabel: "Войти"
+};
 
-export const GET_STATE = 'GET_STATE';
-export const GET_STATE_SUCCESS = 'GET_STATE_SUCCESS';
-export const GET_STATE_FAILED = 'GET_STATE_FAILED';
+import {
+  LOGIN,
+  LOGIN_SUCCESS,
+  LOGIN_FAILED,
+  LOGOUT,
+  LOGOUT_SUCCESS,
+  LOGOUT_FAILED,
+  GET_STATE,
+  GET_STATE_SUCCESS,
+  GET_STATE_FAILED,
+  UPDATE_STATE,
+  UPDATE_STATE_USERNAME,
+  UPDATE_STATE_PASSWORD,
+  UPDATE_STATE_VALID
+} from '../actions/auth';
 
-const initialState = {
-  loggedIn:   false,
-  fetching:   false,
-  profile:    {},
-  contacts:   [],
+import type { Action, State } from '../types/auth'; 
+
+const initialState: State = {
+  loggedIn: false,
+  fetching: false,
+  profile: {},
+  contacts: [],
   navigation: [],
-  message:    {}
-}
+  message: {},
+  labels: { ...DEFAULT_LABELS },
+  loginForm: {
+    username: "",
+    password: "",
+    valid: ""
+  }
+};
 
-export default (state = initialState, action) => {
+export default (state: State = initialState, action: Action) => {
   switch (action.type) {
     case LOGIN:
       return {
         ...state,
         fetching: true
-      }
-  
+      };
+
     case LOGIN_SUCCESS:
+      let loginForm = { ...state.loginForm };
+      loginForm.password = '';
       return {
         ...state,
         fetching: false,
         loggedIn: action.loggedIn,
-        profile:  action.profile,
+        profile: action.profile,
         contacts: action.contacts,
         navigation: action.navigation,
-        message:  action.message
-      }
+        message: action.message,
+        loginForm
+      };
 
     case LOGIN_FAILED:
       return {
         ...state,
         fetching: false,
-        message:  action.message
-      }
+        message: action.message
+      };
 
     case LOGOUT:
       return {
         ...state,
         fetching: true
-      }
-  
+      };
+
     case LOGOUT_SUCCESS:
       return {
         ...state,
         fetching: false,
         loggedIn: false,
-        profile:  {},
-        contacts:   action.contacts,
+        profile: {},
+        contacts: action.contacts,
         navigation: action.navigation,
-        message:  {}
-      }
+        message: {}
+      };
 
     case LOGOUT_FAILED:
       return {
         ...state,
         fetching: false,
-        message:  {}
-      }
+        message: {}
+      };
 
     case GET_STATE:
       return {
         ...state,
         fetching: true
-      }
+      };
 
     case GET_STATE_SUCCESS:
       return {
         ...state,
-        fetching:   false,
-        loggedIn:   action.loggedIn,
-        profile:    action.profile,
-        contacts:   action.contacts,
+        fetching: false,
+        loggedIn: action.loggedIn,
+        profile: action.profile,
+        contacts: action.contacts,
         navigation: action.navigation,
-        message:    {}
-      }
+        message: {},
+        labels: action.labels
+      };
 
     case GET_STATE_FAILED:
       return {
         ...state,
         fetching: false,
         message: {}
+      };
+
+    case UPDATE_STATE:
+      return {
+        ...state
+      };
+
+    case UPDATE_STATE_USERNAME:
+      {
+        let loginForm = { ...state.loginForm };
+        loginForm.username = action.username;
+        return {
+          ...state,
+          loginForm
+        };
+      }
+
+    case UPDATE_STATE_PASSWORD:
+      {
+        let loginForm = { ...state.loginForm };
+        loginForm.password = action.password;
+        return {
+          ...state,
+          loginForm
+        };
+      }
+
+    case UPDATE_STATE_VALID:
+      {
+        let loginForm = { ...state.loginForm };
+        loginForm.valid = action.valid;
+        return {
+          ...state,
+          loginForm
+        };
       }
 
     default:
-      return state
+      return state;
   }
-}
+};
