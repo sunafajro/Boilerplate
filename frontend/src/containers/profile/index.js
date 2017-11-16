3d0bcfc7ce46f050bb7ca539210b07960da34c83
Evgeny Belkin <sunafajro@gmail.com>
2017-11-17 00:18:06 +0300

    в компонент Профиль выведен блок информации по пользователю

--- frontend/src/containers/profile/index.js
+++ frontend/src/containers/profile/index.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React from "react";
+import UserBlock from "../userblock";
 
 class Profile extends React.Component {
-  render () {
+  render() {
     return (
-      <div>
+      <div className="content-top-padding">
+        <div className="row">
+          <div className="col-sm-9">It works!</div>
+          <div className="col-sm-3">
+            <UserBlock />
+          </div>
+        </div>
       </div>
     );
   }
